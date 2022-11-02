9acb34b8ce91fedffa3297fb98db5b7fb74f2f17
ludivinefulop <ludivine_fulop@live.fr>
2022-11-02 14:59:42 +0100

    o

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("JEU DE LA BATAILLE NAVALE");
+        System.out.println("JEU - BATAILLE NAVALE");
         System.out.println("Jeu réalisé par Julie Legrand - Ludivine Fulop - Sara Suleiman");
         Menu M = new Menu();
         M.MenuDebut();
