5d86903740402f22c6ecc65a14b5fc8903c22ba0
ludivinefulop <ludivine_fulop@live.fr>
2022-11-02 15:09:24 +0100

    o

---
+++ src/main/java/org/example/JoueurOrdi.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class JoueurOrdi {
+
+    private String pseudo;
+    private int score;
+
+    //Déclaration du constructeur
+    public JoueurOrdi(String pseudo, int score) {
+        this.pseudo = pseudo;
+        this.score = score;
+    }
+
+    //Déclaration des getters
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    //Déclaration des setters
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
