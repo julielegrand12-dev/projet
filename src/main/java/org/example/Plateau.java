87e28170db33a6f47de1c6f3a5046c73abdbf1a7
Julie Legrand <julie.legrand@edu.ece.fr>
2022-11-09 11:07:25 +0100

    plateau graphique

--- src/main/java/org/example/Plateau.java
+++ src/main/java/org/example/Plateau.java
@@ -1,11 +1,46 @@
 package org.example;
+import javax.swing.*;
+import java.awt.*;
 
-public class Plateau {
+
+public class Plateau extends JPanel {
+
+    public void Fenetre()
+    {
+        JFrame fen = new JFrame();
+        fen.setVisible(true); //pour ouvrir la fenetre
+        fen.setSize(600,600);
+        fen.setDefaultCloseOperation(fen.EXIT_ON_CLOSE);
+        fen.setTitle("Fenetre 1");
+        fen.add(this);
+        //fen.setContentPane(fen);
+        JLabel jla = new JLabel("Joueur 1");
+        fen.add(jla);
+
+
+    }
+    public void paintComponent(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+
+        for(int i=50; i<550;i++ )
+        {
+            g.drawLine(i,50,i,550);
+            g.drawLine(50,i,550,i);
+            i=i+30;
+        }
+
+    }
+
+    private void setBackgroud(Color blue) {
+    }
+
     private int nbligne;
     private int nbcol;
     private char[][] grille;
     public void plateau(int n,int p)
     {
+        Fenetre();
         nbligne=n;
         nbcol=p;
         grille = new char[nbligne][nbcol];
