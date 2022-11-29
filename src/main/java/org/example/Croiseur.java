663304ac829a2205146c6510b33d9b2213e7a75c
Sara <s4r4.slm@outlook.com>
2022-11-29 19:52:15 +0100

    Ajout sous-classes bateau

---
+++ src/main/java/org/example/Croiseur.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Croiseur extends Bateau{
+    public Croiseur( int taille_navire,int id_navire, String nom_navire, boolean Horizontal, int coordonneeDebutX, int coordonneeDebutY){
+        super(taille_navire,id_navire,nom_navire,Horizontal,coordonneeDebutX,coordonneeDebutY);
+    }
+
+    public void Attaque(){
+
+    }
+
+}
